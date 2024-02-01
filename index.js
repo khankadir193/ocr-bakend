31f7b650f2af72cb793e95fc9cef64251122c57b
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-01 15:21:13 +0530

    extracting data from image

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 // const Tesseract = require('tesseract.js');
 const app = express();
 app.use(express.json());
-app.post('/perform-ocr',(req,res)=>{
-    const imgUrl = req.body.imgUrl;
-    
-    console.log('req.body....????',req.body);
-    console.log('imgUrl...??',imgUrl);
-    let recognitionText = "router is working properly.. OCR response..";  
+app.post('/perform-ocr', async (req, res) => {
+    // const imgUrl = req.body.imgUrl;
+    const ocrApi = "https://api.ocr.space/parse/imageurl?apikey=K89496159888957&url=https://hamariweb.com/poetry/images/Poetry/english-poetry-100696.png";
 
+    // let recognitionText = "router is working properly.. OCR response..";
     res.header('Access-Control-Allow-Origin', '*');
-    return res.json({"text":`${recognitionText} imageURL ${imgUrl}`}); 
+    // return res.json({ "text": `${recognitionText} imageURL ${imgUrl}` });
+
+    const response = await axios.get(ocrApi);
+    console.log('response...????', response.data.ParsedResults[0].ParsedText);
+
+    return res.json({ "text": `${response.data.ParsedResults[0].ParsedText}` });
+    
 });
 
-const  PORT = 3000;
+const PORT = 3000;
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log(`Server is running is on port ${PORT}`);
 });
